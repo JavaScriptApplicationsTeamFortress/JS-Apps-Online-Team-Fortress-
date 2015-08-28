bd926fb8241c229b25f9baef9318104843b697bb
pmechkova <pmechkova@gmail.com>
2015-08-28 15:19:46 +0300

    added CLetter view

---
+++ CV-Generator Project/CV-Generator/scripts/Model/coverLetter-view.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    var i,
+        len,
+        fields = [{
+            name: 'First name',
+            id: 'first-name',
+            data: null
+        }, {
+            name: 'Last name',
+            id: 'last-name',
+            data: null
+        },  {
+            name: 'Telephone',
+            id: 'phone',
+            src: null,
+            data: null
+        }, {
+            name: 'Address',
+            id: 'Address',
+            src: null,
+            data: null
+        }],
+        wrraper;
+
+    var coverLetterForm = $("#cover-letter-form")
+
+    $("#target").on("submit", function(event) {
+        event.preventDefault();
+    });
+    });
+});
