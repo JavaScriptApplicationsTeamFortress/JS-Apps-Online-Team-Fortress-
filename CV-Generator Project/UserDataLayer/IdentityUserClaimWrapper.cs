b1bcfd3b3167695b2b5766ca2f450cb021d8abca
Iliya Nedelchev <Il.Nedelchev@gmail.com>
2015-08-30 22:34:37 +0300

    Controller

---
+++ CV-Generator Project/UserDataLayer/IdentityUserClaimWrapper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDataLayer
+{
+    public class IdentityUserClaimWrapper
+    {
+        public IdentityUserClaimWrapper()
+        {
+        }
+
+        public IdentityUserClaimWrapper(IdentityUserClaim userClaim)
+        {
+            this.Id = userClaim.Id;
+            this.UserId = userClaim.UserId;
+            this.ClaimValue = userClaim.ClaimValue;
+            this.ClaimType = userClaim.ClaimType;
+        }
+
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string ClaimValue { get; set; }
+        public string ClaimType { get; set; }
+
+        public IdentityUserClaim Convert()
+        {
+            var userClaim = new IdentityUserClaim();
+            userClaim.Id = this.Id;
+            userClaim.UserId = this.UserId;
+            userClaim.ClaimValue = this.ClaimValue;
+            userClaim.ClaimType = this.ClaimType;
+
+            return userClaim;
+        }
+    }
+}
