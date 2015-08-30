473c6c7255afb03ccea7b1c4c3661e237f456a7c
tddold <tdd@dekom.bg>
2015-08-30 21:22:41 +0300

    Install packages Mocha, Chai and SinonJS

---
+++ CV-Generator Project/CV-Generator/Controllers/MochaController.cs
@@ -0,0 +1,14 @@
+#if DEBUG
+namespace CV_Generator.Controllers
+{
+    using System.Web.Mvc;
+
+    public class MochaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+#endif
