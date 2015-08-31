17789df7d5b716c1d3c7cf171f23ffb838c08740
ivanindzhov <ivanindzhov@gmail.com>
2015-08-31 12:35:14 +0300

    add indicationCurrentPage.js

---
+++ CV-Generator Project/CV-Generator/scripts/Model/indicationCurrentPage.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    var currentLocation = document.location.toString();
+    var n = currentLocation.lastIndexOf("/");
+    var result = currentLocation.substring(n);
+
+    if (result === "/") {
+        $('#home').attr('class', 'selected');
+    }
+
+    if (result === "/CreateCV") {
+        $('#addCv').attr('class', 'selected');
+    }
+    if (result === "/PreviewDocument") {
+        $('#prevDocument').attr('class', 'selected');
+    }
+});
